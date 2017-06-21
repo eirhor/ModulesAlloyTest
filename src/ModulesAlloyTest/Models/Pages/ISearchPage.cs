92d72b08918d178d7f73f35192605c6a189769ac
Eirik Horvath <eirik@geta.no>
2017-06-21 21:15:20 +0200

    init

---
+++ src/ModulesAlloyTest/Models/Pages/ISearchPage.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModulesAlloyTest.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
